5fbac68f73a65b9bb57d2312e7e0b8fb1522311e
Benjamin Croad <benjamincroadapple@gmail.com>
2022-09-25 10:26:48 +0100

    Added seeds for events

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+require "date"
+
+puts "Destroying existing data..."
+Event.destroy_all
+
+puts "Generating seeds..."
+
+puts "Generating events..."
+20.times do
+  rand_price = rand(0..200)
+  event = Event.new(
+    title: Faker::Commerce.product_name,
+    description: Faker::Hobby.activity,
+    price: rand_price,
+    date: Date.today
+  )
+  event.save!
+end
+
+puts "Seeds complete!"
