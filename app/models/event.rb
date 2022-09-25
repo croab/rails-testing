08ef2702cfebd109afcffc56a6481b2e93b5c087
Benjamin Croad <benjamincroadapple@gmail.com>
2022-09-25 11:11:01 +0100

    Testing nearly complete, validations need adding to events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  validates :title, :description, :price, :date, presence: true
+  validates :title, length: { minimum: 2, maximum: 50 }
+  validates :description, length: { minimum: 10, maximum: 200 }
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -12,78 +12,86 @@ RSpec.describe Event, type: :model do
   before { subject.save! }
 
   # Tests
-  it "should have a title" do
-    subject.title = nil
-    expect(subject).to_not be_valid
+  describe "title" do
+    it "should have a title" do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "can have a title with 2 characters" do
+      subject.title = "A"*2
+      expect(subject).to be_valid
+    end
+
+    it "should have a title longer than 2 characters" do
+      subject.title = "A"
+      expect(subject).to_not be_valid
+    end
+
+    it "can have a title with 50 characters" do
+      subject.title = "A" * 50
+      expect(subject).to be_valid
+    end
+
+    it "should have a title <= 50 characters" do
+      subject.title = "A" * 51
+      expect(subject).to_not be_valid
+    end
   end
 
-  it "can have a title with 2 characters" do
-    subject.title = "A"*2
-    expect(subject).to be_valid
+  describe "description" do
+    it "should have a description" do
+      subject.description = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "can have a description of 10 characters" do
+      subject.description = "A" * 10
+      expect(subject).to be_valid
+    end
+
+    it "should have a description >= 10 characters" do
+      subject.description = "A" * 9
+      expect(subject).to_not be_valid
+    end
+
+    it "should have a description <= 200 characters" do
+      subject.description = "A" * 201
+      expect(subject).to_not be_valid
+    end
   end
 
-  it "should have a title longer than 2 characters" do
-    subject.title = "A"
-    expect(subject).to_not be_valid
+  describe "price" do
+    it "should have a price" do
+      subject.price = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "should have a price than is an integer" do
+      subject.price = "Hi"
+      expect(subject).to_not be_valid
+    end
+
+    it "should have a price >= 0" do
+      subject.price = -1
+      expect(subject).to_not be_valid
+    end
+
+    it "should have a price <= 200" do
+      subject.price = 201
+      expect(subject).to_not be_valid
+    end
   end
 
-  it "can have a title with 50 characters" do
-    subject.title = "A" * 50
-    expect(subject).to be_valid
-  end
+  describe "date" do
+    it "should have a date" do
+      subject.date = nil
+      expect(subject).to_not be_valid
+    end
 
-  it "should have a title <= 50 characters" do
-    subject.title = "A" * 51
-    expect(subject).to_not be_valid
-  end
-
-  it "should have a description" do
-    subject.description = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "can have a description of 10 characters" do
-    subject.description = "A" * 10
-    expect(subject).to be_valid
-  end
-
-  it "should have a description >= 10 characters" do
-    subject.description = "A" * 9
-    expect(subject).to_not be_valid
-  end
-
-  it "should have a description <= 200 characters" do
-    subject.description = "A" * 201
-    expect(subject).to_not be_valid
-  end
-
-  it "should have a price" do
-    subject.price = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "should have a price than is an integer" do
-    subject.price = "Hi"
-    expect(subject).to_not be_valid
-  end
-
-  it "should have a price >= 0" do
-    subject.price = -1
-    expect(subject).to_not be_valid
-  end
-
-  it "should have a price <= 200" do
-    subject.price = 201
-    expect(subject).to_not be_valid
-  end
-
-  it "should have a date" do
-    subject.date = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "should have a date after the present date" do
-    subject.date = Date.today - 1
-    expect(subject).to_not be_valid
+    it "should have a date after the present date" do
+      subject.date = Date.today - 1
+      expect(subject).to_not be_valid
+    end
   end
 end
