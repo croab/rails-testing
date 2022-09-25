b3dbc57ede892394a4bbf66cab293b83c06b44d6
Benjamin Croad <benjamincroadapple@gmail.com>
2022-09-25 10:49:24 +0100

    Added tests to events

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -12,16 +12,78 @@ RSpec.describe Event, type: :model do
   before { subject.save! }
 
   # Tests
-  it "should have a title"
-  it "should have a title longer than 2 characters"
-  it "should have a title less than 50 characters"
-  it "should have a description"
-  it "should have a description longer than 10 characters"
-  it "should have a description less than 200 characters"
-  it "should have a price"
-  it "should have a price than is an integer"
-  it "should have a price greater than 0"
-  it "should have a price less than 200"
-  it "should have a date"
-  it "should have a date after the present date"
+  it "should have a title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "can have a title with 2 characters" do
+    subject.title = "A"*2
+    expect(subject).to be_valid
+  end
+
+  it "should have a title longer than 2 characters" do
+    subject.title = "A"
+    expect(subject).to_not be_valid
+  end
+
+  it "can have a title with 50 characters" do
+    subject.title = "A" * 50
+    expect(subject).to be_valid
+  end
+
+  it "should have a title <= 50 characters" do
+    subject.title = "A" * 51
+    expect(subject).to_not be_valid
+  end
+
+  it "should have a description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "can have a description of 10 characters" do
+    subject.description = "A" * 10
+    expect(subject).to be_valid
+  end
+
+  it "should have a description >= 10 characters" do
+    subject.description = "A" * 9
+    expect(subject).to_not be_valid
+  end
+
+  it "should have a description <= 200 characters" do
+    subject.description = "A" * 201
+    expect(subject).to_not be_valid
+  end
+
+  it "should have a price" do
+    subject.price = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "should have a price than is an integer" do
+    subject.price = "Hi"
+    expect(subject).to_not be_valid
+  end
+
+  it "should have a price >= 0" do
+    subject.price = -1
+    expect(subject).to_not be_valid
+  end
+
+  it "should have a price <= 200" do
+    subject.price = 201
+    expect(subject).to_not be_valid
+  end
+
+  it "should have a date" do
+    subject.date = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "should have a date after the present date" do
+    subject.date = Date.today - 1
+    expect(subject).to_not be_valid
+  end
 end
