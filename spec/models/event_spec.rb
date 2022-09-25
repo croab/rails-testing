37c601276c640bacfcefe8b84d5cc3814706c896
Benjamin Croad <benjamincroadapple@gmail.com>
2022-09-25 10:36:08 +0100

    Added pending tests for events model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    Event.new(
+      title: "New Event",
+      description: "Good event!",
+      price: 50,
+      date: "Sun, 25 Sep 2022"
+    )
+  }
+  before { subject.save! }
+
+  # Tests
+  it "should have a title"
+  it "should have a title longer than 2 characters"
+  it "should have a title less than 50 characters"
+  it "should have a description"
+  it "should have a description longer than 10 characters"
+  it "should have a description less than 200 characters"
+  it "should have a price"
+  it "should have a price than is an integer"
+  it "should have a price greater than 0"
+  it "should have a price less than 200"
+  it "should have a date"
+  it "should have a date after the present date"
 end
